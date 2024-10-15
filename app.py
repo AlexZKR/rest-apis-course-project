e7dae964814bfd887f8025764b3a057f187144d2
Alex <zakrevskyalex1323@gmail.com>
2024-10-15 14:02:37 +0300

    deploy tweaks

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask
 from flask_smorest import Api
 from db import db
-from config.config import Config  # Import configuration
 from config.jwt_config import configure_jwt  # Import JWT configuration
 from flask_migrate import Migrate
-
+from dotenv import load_dotenv
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -14,17 +14,26 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
-
+    load_dotenv()
     # Load config from the Config class
-    app.config.from_object(Config)
-
-    # Override the SQLALCHEMY_DATABASE_URI if db_url is provided
-    if db_url:
-        app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = (
+        "https://cdn.jsdelivr.net/npm/swagger-ui/dist/"
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "52367737184688380116997772023396127865"
 
     db.init_app(app)
 
-    #not needed because tables are created with migrate
+    # not needed because tables are created with migrate
     # with app.app_context():
     #     db.create_all()
 
@@ -32,7 +41,7 @@ def create_app(db_url=None):
 
     # Apply the JWT configurations
     configure_jwt(app)
-    
+
     Migrate(app, db)
 
     api.register_blueprint(ItemBlueprint)
